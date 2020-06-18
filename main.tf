4cff13eadee17179c75603dad98a326634593640
hoodbu <38543715+hoodbu@users.noreply.github.com>
2020-06-18 18:05:33 -0400

    Initial upload

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  profile    = "default"
+  region     = var.region
+}
+
+resource "aws_instance" "example" {
+  ami           = var.amis[var.region]
+  instance_type = "t2.micro"
+}
+
+resource "aws_eip" "ip" {
+    vpc = true
+    instance = aws_instance.example.id
+}
