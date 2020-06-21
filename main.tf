1e6dcebde9d7222914218246d9435e9b65ee1d74
hoodbu <38543715+hoodbu@users.noreply.github.com>
2020-06-20 21:30:38 -0400

    Update main.tf
    Added IGW

--- main.tf
+++ main.tf
@@ -8,6 +8,25 @@ resource "aws_instance" "example" {
   instance_type = "t2.micro"
   vpc_security_group_ids=["sg-06f7d42f923804e56"]
   subnet_id="subnet-0d5dfc3739ac57429"
+    tags {
+        Name = "Pakdude Instance"
+    }
+}
+
+resource "aws_vpc" "vpc" {
+    cidr_block           = "10.0.0.0/16"
+    enable_dns_support   = true
+    enable_dns_hostnames = true
+    tags {
+        Name = "Staging VPC"
+    }
+}
+
+resource "aws_internet_gateway" "igw" {
+    vpc_id = "${aws_vpc.vpc.id}"
+    tags {
+        Name = "Staging IGW"
+    }
 }
 
 resource "aws_eip" "ip" {
