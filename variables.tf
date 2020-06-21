0d48b735446e7d372bb25335733cb91ee1e1287d
hoodbu <38543715+hoodbu@users.noreply.github.com>
2020-06-21 17:14:41 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,9 +10,7 @@ variable "amis" {
   }
 }
 
-data "aws_availability_zones" "available" {
-    state = "available"
-}
+data "aws_availability_zones" "available" {}
 
 # Networking
 variable "aws_vpc_cidr" {}
