7f7cb2844a10da3598a24ddee6442d6f7b574491
hoodbu <38543715+hoodbu@users.noreply.github.com>
2020-06-20 21:34:55 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "example" {
   instance_type = "t2.micro"
   vpc_security_group_ids=["sg-06f7d42f923804e56"]
   subnet_id="subnet-0d5dfc3739ac57429"
-    tags {
+    tags = {
         Name = "Pakdude Instance"
     }
 }
@@ -17,14 +17,14 @@ resource "aws_vpc" "vpc" {
     cidr_block           = "10.0.0.0/16"
     enable_dns_support   = true
     enable_dns_hostnames = true
-    tags {
+    tags = {
         Name = "Staging VPC"
     }
 }
 
 resource "aws_internet_gateway" "igw" {
     vpc_id = "aws_vpc.vpc.id"
-    tags {
+    tags = {
         Name = "Staging IGW"
     }
 }
